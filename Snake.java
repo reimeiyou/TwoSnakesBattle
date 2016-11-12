da2b0887e9b8fce6750fb3c58583a73c3ce582bb
reimeiyou <georgelmy@pku.edu.cn>
2016-11-12 14:56:48 -0500

    first commit: UI framework

---
+++ Snake.java
@@ -0,0 +1,25 @@
+package homework9;
+
+public class Snake {
+	public static void main(String args[]) {
+		mainframe f = new mainframe();
+		mainframe.init();
+		while (true) {
+			while (mainframe.running && !mainframe.pausing) {
+				f.move();
+	//			for(int i=0;i<mainframe.framewidth;i++){
+	//				for(int j=0;j<mainframe.framelength;j++){
+	//					System.out.print(""+mainframe.state[i][j]);
+	//				}
+	//				System.out.println();
+	//			}
+	//			System.out.println(mainframe.snakeLength);
+				
+				try {
+					Thread.sleep(mainframe.waittime);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+}
